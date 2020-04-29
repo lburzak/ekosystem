455af7df330eba6c897ada1827b80f330467004d
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2020-04-29 18:47:41 +0200

    Turn off ekolog

--- src/domain/logging/ekolog.cpp
+++ src/domain/logging/ekolog.cpp
@@ -15,21 +15,26 @@ Ekolog& Ekolog::getInstance() {
 }
 
 void Ekolog::organism(Organism& organism) {
+    return;
     cout << organism.getId() << "\t" << organism.bundleInfo().toString() << endl;
 }
 
 void Ekolog::decomposed(Organism& organism) {
+    return;
     cout << "DECOMPOSED\tID=" << organism.getId() << endl;
 }
 
 void Ekolog::tick(Organism& organism) {
+    return;
     cout << "TICK\t\tID=" << organism.getId() << "\t" << organism.bundleInfo().toString() << endl;
 }
 
 void Ekolog::decomposing(Organism &organism, unsigned fromMass, unsigned toMass) {
+    return;
     cout << "DECOMPOSING\tID=" << organism.getId() << "\t" << fromMass << " -> " << toMass << endl;
 }
 
 void Ekolog::death(Organism &organism) {
+    return;
     cout << "DEATH\t\tID=" << organism.getId() << endl;
 }
